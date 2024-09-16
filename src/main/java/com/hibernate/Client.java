e2dc3f4da0515a62ab3c03027863e8cdf892e363
pancham01 <pancham9557@gmail.com>
2024-09-16 10:23:46 +0530

    Initial commit

---
+++ src/main/java/com/hibernate/Client.java
@@ -0,0 +1,30 @@
+package com.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.entity.Employee;
+
+public class Client {
+
+	public static void main(String[] args) {
+		Employee e = new Employee("Swati", "female", 45784);
+
+		 
+		SessionFactory sf = new MetadataSources(new StandardServiceRegistryBuilder().configure().build()).getMetadataBuilder().build().buildSessionFactory();
+//		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		Session session = sf.openSession();
+		Transaction tx = session.beginTransaction();
+//		session.save(e);
+//		tx.commit();
+		Employee employee = session.load(Employee.class, 2);
+		System.out.println(employee);
+	}
+
+}
